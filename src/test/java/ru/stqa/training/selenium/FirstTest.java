cc4258b14cda62d60fe010dcc04e422ea3b29fae
aida <sadykova.aida91@gmail.com>
2018-03-11 15:12:31 +0600

    commit 1

---
+++ src/test/java/ru/stqa/training/selenium/FirstTest.java
@@ -0,0 +1,16 @@
+package ru.stqa.training.selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+    @Test
+    public void firstTest() {
+        System.setProperty("webdriver.chrome.driver", "/usr/local/share/chromedriver");
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        driver.quit();
+    }
+}
