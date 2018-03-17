9d0a5a1f59cfcd7e4245c38149db0e04d1b1e26f
aida <sadykova.aida91@gmail.com>
2018-03-17 23:06:39 +0600

    - добавлен последовательный click() по всем разделам меню, включая подменю - добавлена проверка наличия заголовка (h1)

--- src/test/java/ru/stqa/training/selenium/RunMozilla.java
+++ src/test/java/ru/stqa/training/selenium/RunMozilla.java
@@ -5,11 +5,18 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.NoSuchElementException;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import static org.junit.Assert.assertTrue;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
@@ -18,6 +25,19 @@ public class RunMozilla {
     private WebDriver driver;
     private WebDriverWait wait;
 
+    private boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException ex) {
+            return false;
+        }
+    }
+
+    /*public boolean areElementsPresent(WebDriver driver, By locator) {
+        return driver.findElements(locator).size() > 0;
+    }*/
+
     @Before
     public void start() {
 
@@ -25,6 +45,7 @@ public class RunMozilla {
                 .setLegacy(false);
 
         driver = new FirefoxDriver(options); /*init драйвера */
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
 
@@ -36,6 +57,28 @@ public class RunMozilla {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
+
+        WebElement box = driver.findElement(By.cssSelector("#box-apps-menu"));
+        List<WebElement> apps = box.findElements(By.cssSelector("li#app-"));
+
+        int i, j;
+        for (i = 0; i < apps.size(); i++)
+        {
+            box = driver.findElement(By.cssSelector("#box-apps-menu"));
+            apps = box.findElements(By.cssSelector("li#app-"));
+            apps.get(i).click();
+            assertTrue(isElementPresent(By.cssSelector("h1")));
+            box = driver.findElement(By.cssSelector("#box-apps-menu"));
+            apps = box.findElements(By.cssSelector("li#app-"));
+            List<WebElement> docs = box.findElements(By.cssSelector("li#app- [id^=doc]"));
+            for (j = 0; j < docs.size(); j++)
+            {
+                box = driver.findElement(By.cssSelector("#box-apps-menu"));
+                docs = box.findElements(By.cssSelector("li#app- [id^=doc]"));
+                docs.get(j).click();
+                assertTrue(isElementPresent(By.cssSelector("h1")));
+            }
+        }
     }
 
     @After
