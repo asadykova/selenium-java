1146a12f849ace531ad275aaadf8782594c8f224
aida <sadykova.aida91@gmail.com>
2018-03-28 21:40:06 +0600

    доработан класс для создания аккаунта

--- src/test/java/ru/stqa/training/selenium/CreateAccount.java
+++ src/test/java/ru/stqa/training/selenium/CreateAccount.java
@@ -7,11 +7,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
-import javax.swing.*;
 import java.util.Date;
 import java.util.List;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
@@ -29,31 +26,38 @@ public class CreateAccount extends MainClass {
         List<WebElement> account = driver.findElements(By.cssSelector("#create-account div.content tr"));
         WebElement firstName = account.get(1).findElement(By.cssSelector("[name=firstname]"));
         firstName.sendKeys("Aida" + Keys.TAB + "Sadykova" + Keys.TAB
-        + "Abay st." + Keys.TAB + Keys.TAB + "00057" + Keys.TAB + "New York"
-        + Keys.TAB + Keys.ENTER + "United States" + Keys.ENTER);
+        + "Abay st." + Keys.TAB + Keys.TAB + "00057" + Keys.TAB + "New York");
+        WebElement country = driver.findElement(By.cssSelector(".select2"));
+        country.click();
+        WebElement ff = driver.findElement(By.cssSelector(".select2-search__field"));
+        ff.sendKeys("United States" + Keys.ENTER);
+        WebElement state = account.get(4).findElement(By.cssSelector("select[name=zone_code]"));
+        Select stateName = new Select(state);
+        stateName.selectByVisibleText("Arizona");
 
-        /*WebElement state = account.get(4).findElement(By.cssSelector("select[name=zone_code]"));
-        //state.isDisplayed();
-        state.click();
-        Actions mouse = new Actions(driver);
-        WebElement stateName = state.findElement(By.cssSelector("option[value=AZ]"));
-        System.out.print(stateName.getText());
-        mouse.
-                moveToElement(stateName).
-                sendKeys(Keys.ENTER).
-                perform();
 
-*/
         WebElement email = account.get(5).findElement(By.cssSelector("[name=email]"));
         Date d = new Date();
-        String randomString = "asadykova" + d.getYear() + d.getDate() + d.getTime() + "@mail.com";
+        String randomString = "asadykova_" + d.getDate() + d.getTime() + "@mail.com";
         System.out.print(randomString);
         email.sendKeys(randomString + Keys.TAB + "+77777777777" + Keys.TAB
-        + Keys.SPACE + Keys.TAB + "123456" + Keys.TAB + "123456" + Keys.TAB /*+ Keys.ENTER*/);
-        /*
-        "#create-account div.content tr select [name=zone_code]" //выбрать любую зону
-        в java.io.File есть getCanonicalPath()
-        http://phpfaq.ru/newbie/paths
-        */
+        + Keys.SPACE + Keys.TAB + "123456" + Keys.TAB + "123456" + Keys.TAB + Keys.ENTER);
+
+        //выход
+        WebElement logOut = driver.findElement(By.cssSelector("#box-account div.content li:last-child a"));
+        Actions mouse = new Actions(driver);
+        mouse.moveToElement(logOut).click().build().perform();
+        //logOut.click(); //просто клик без actions не работал для фаерфокс почему-то...
+
+        //авторизация
+        WebElement login = driver.findElement(By.cssSelector("#box-account-login [name=email]"));
+        login.sendKeys(randomString + Keys.TAB + "123456");
+
+        WebElement loginButton =  driver.findElement(By.cssSelector("#box-account-login [name=login]"));
+        loginButton.click();
+
+        //снова выход
+        logOut = driver.findElement(By.cssSelector("#box-account div.content li:last-child a"));
+        mouse.moveToElement(logOut).click().build().perform();
     }
 }
--- src/test/java/ru/stqa/training/selenium/MainClass.java
+++ src/test/java/ru/stqa/training/selenium/MainClass.java
@@ -19,11 +19,11 @@ public class MainClass {
     @Before
     public void start() {
 
-       /* FirefoxOptions options = new FirefoxOptions()
+        FirefoxOptions options = new FirefoxOptions()
                 .setLegacy(false);
 
         driver = new FirefoxDriver(options); /*init драйвера */
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
